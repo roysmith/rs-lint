8fd577dad328a9998ee44d509e68903ce4502663
Roy Smith <roy@panix.com>
2024-03-26 20:22:16 -0400

    mwparserfromhell

--- rs_lint/article.py
+++ rs_lint/article.py
@@ -1,6 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+import mwparserfromhell as mwp
+
 
 
 @dataclass
 class Article:
     text: str
+    code: mwp.wikicode.Wikicode = field(init=False)
+
+    def __post_init__(self):
+        self.code = mwp.parse(self.text)
--- tests/rs_lint/test_article.py
+++ tests/rs_lint/test_article.py
@@ -4,3 +4,4 @@ from rs_lint import Article
 def test_construct():
     article = Article("")
     assert article.text == ""
+    assert article.code == ""
