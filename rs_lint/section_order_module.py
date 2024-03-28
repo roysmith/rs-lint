69bf0c424c6c37d5c53b6b9ac5ea6676556c4e37
Roy Smith <roy@panix.com>
2024-03-28 00:15:09 -0400

    Get most of pre-content order checking working

--- rs_lint/section_order_module.py
+++ rs_lint/section_order_module.py
@@ -1,13 +1,66 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import Self
 
 from mwparserfromhell.wikicode import Wikicode
+from mwparserfromhell.nodes import Text, Template
 
 from rs_lint import LinterModule, Article
 
 
+class TemplateType(Enum):
+    """See [[en:MOS:ORDER]]"""
+
+    SHORT_DESCRIPTION = 1
+    TITLE_MODIFIER = 2
+    HATNOTE = 3
+    ARTICLE_STATUS = 4
+    DELETION_PROTECTION_TAG = 5
+    MAINTENANCE_TAG = 6
+    ENGLISH_VARIANT = 7
+    DATE_FORMAT = 7
+    INFOBOX = 8
+    LANGUAGE_MAINTENANCE = 9
+    IMAGE = 10  # Not really a template, but pretending it is simplifies things
+    NAVIGATION_HEADER = 11
+
+
 @dataclass
 class SectionOrderModule(LinterModule):
-    def get_pre_content(self: Self, article: Article) -> Wikicode:
+    def get_pre_content_code(self: Self, article: Article) -> Wikicode:
         sections = article.code.get_sections()
         return sections[0]
+
+    def get_pre_content_templates(self: Self, article: Article):
+        code = self.get_pre_content_code(article)
+        for node in code.nodes:
+            if isinstance(node, Text) and str(node).strip() == "":
+                continue
+            if isinstance(node, Template):
+                yield node
+            else:
+                return
+
+    def is_short_description(self: Self, template: Template):
+        return template.name.matches("short description")
+
+    def one_of(self: Self, template: Template, names):
+        for name in names:
+            if template.name.matches(name):
+                return True
+        return False
+
+    def classify_template(self: Self, template: Template):
+        if self.one_of(template, ["short description", "shortdescription"]):
+            return TemplateType.SHORT_DESCRIPTION
+        if self.one_of(template, ["featured article", "featured list", "good article"]):
+            return TemplateType.ARTICLE_STATUS
+        if self.one_of(template, ["use mdy dates", "use dmy dates"]):
+            return TemplateType.DATE_FORMAT
+        if template.name.lower().startswith("infobox"):
+            return TemplateType.INFOBOX
+        return None
+
+    def get_pre_content_template_types(self, article):
+        for template in self.get_pre_content_templates(article):
+            yield self.classify_template(template)
--- tests/rs_lint/test_section_order_module.py
+++ tests/rs_lint/test_section_order_module.py
@@ -1,9 +1,15 @@
 from textwrap import dedent
 
 import pytest
-from mwparserfromhell.wikicode import Wikicode
+from mwparserfromhell.wikicode import Wikicode, Template
 
 from rs_lint import SectionOrderModule, Article
+from rs_lint.section_order_module import TemplateType
+
+
+@pytest.fixture
+def module():
+    return SectionOrderModule()
 
 
 def make_article(text: str) -> Article:
@@ -20,8 +26,7 @@ def make_article(text: str) -> Article:
     return Article(dedent(text))
 
 
-def test_construct():
-    module = SectionOrderModule()
+def test_construct(module):
     assert module.name == "SectionOrder"
 
 
@@ -46,9 +51,82 @@ def test_construct():
         ),
     ],
 )
-def test_get_pre_content(text, expected_result):
-    module = SectionOrderModule()
+def test_get_pre_content_code(module, text, expected_result):
     article = Article(text)
-    result = module.get_pre_content(article)
+    result = module.get_pre_content_code(article)
     assert isinstance(result, Wikicode)
     assert result == expected_result
+
+
+def test_get_pre_content_templates(module):
+    article = make_article(
+        """\
+            {{short description|American aviator (1916–2019)}}
+            {{Featured article}}
+
+            {{Use mdy dates|date=July 2023}}
+            {{Infobox aviator
+            | image       = File:WASP Dorothy Kocher Olsen.JPG
+            |caption     = Kocher {{circa|1943}} (U.S. Air Force photo).
+            | name        = Dorothy Olsen
+            | birth_name  = Dorothy Eleanor Kocher
+            | birth_date  = {{birth date|1916|7|10}}
+            | birth_place = [[Woodburn, Oregon]], U.S.
+            | death_date  = {{death date and age|2019|7|23|1916|7|10}}
+            | death_place = [[University Place, Washington]], U.S.
+            | nationality = <!-- use only when necessary per [[WP:INFONAT]] -->
+            | known for   = Member of [[Women Airforce Service Pilots]] (WASP)
+            |alt=Portrait of Dorothy Olsen wearing a World-War II style bomber jacket.}}
+            '''Dorothy Eleanor Olsen''' ({{née|'''Kocher'''}}; July 10, 1916 – July 23, 2019)
+            was an American aircraft pilot and member of the [[Women Airforce Service Pilots]]
+            (WASPs) during[[World War II]].  She grew up on her family's farm in
+            [[Woodburn, Oregon]], developing an interest in aviation at a young age.
+            She earned her [[Private pilot licence|private pilot's license]] in 1939,
+            when it was unusual for women to be pilots. 
+            """
+    )
+    templates = list(module.get_pre_content_templates(article))
+    assert len(templates) == 4
+
+
+@pytest.mark.parametrize(
+    "name, template_type",
+    [
+        ("short description", TemplateType.SHORT_DESCRIPTION),
+        ("shortdescription", TemplateType.SHORT_DESCRIPTION),
+    ],
+)
+def test_classify_template(module, name, template_type):
+    template = Template(name)
+    assert module.classify_template(template) == template_type
+
+
+def test_get_pre_content_template_types(module):
+    article = make_article(
+        """\
+            {{short description|American aviator (1916–2019)}}
+            {{Featured article}}
+
+            {{Use mdy dates|date=July 2023}}
+            {{Infobox aviator
+            | image       = File:WASP Dorothy Kocher Olsen.JPG
+            |caption     = Kocher {{circa|1943}} (U.S. Air Force photo).
+            | name        = Dorothy Olsen
+            | birth_name  = Dorothy Eleanor Kocher
+            | birth_date  = {{birth date|1916|7|10}}
+            | birth_place = [[Woodburn, Oregon]], U.S.
+            | death_date  = {{death date and age|2019|7|23|1916|7|10}}
+            | death_place = [[University Place, Washington]], U.S.
+            | nationality = <!-- use only when necessary per [[WP:INFONAT]] -->
+            | known for   = Member of [[Women Airforce Service Pilots]] (WASP)
+            |alt=Portrait of Dorothy Olsen wearing a World-War II style bomber jacket.}}
+            '''Dorothy Eleanor Olsen''' ({{née|'''Kocher'''}}; July 10, 1916 – July 23, 2019)
+            was an American aircraft pilot and member of the [[Women Airforce Service Pilots]]
+            (WASPs) during[[World War II]].  She grew up on her family's farm in
+            [[Woodburn, Oregon]], developing an interest in aviation at a young age.
+            She earned her [[Private pilot licence|private pilot's license]] in 1939,
+            when it was unusual for women to be pilots. 
+            """
+    )
+    values = [t.value for t in module.get_pre_content_template_types(article)]
+    assert sorted(values) == values
